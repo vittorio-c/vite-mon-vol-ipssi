937e047d25482e7fe736191d27a270be33453b3f
eltov <dev@eltov.xyz>
2022-01-14 09:45:35 +0100

    First commit

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+//site name
+const SITE_NAME = 'your-site-name';
+
+//App Root
+define('APP_ROOT', dirname(__FILE__, 2));
+const URL_ROOT = '/';
+const URL_SUBFOLDER = '';
+
+//DB Params
+const DB_HOST = 'your-host';
+const DB_USER = 'your-username';
+const DB_PASS = 'your-password';
+const DB_NAME = 'your-db-name';
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+// Routes system
+$routes = new RouteCollection();
+$routes->add('homepage',
+    new Route(
+        constant('URL_SUBFOLDER').'/',
+        ['controller' => 'PageController', 'method' => 'indexAction'],
+        []
+    ));
+$routes->add('product',
+    new Route(
+        constant('URL_SUBFOLDER').'/product/{id}',
+        ['controller' => 'ProductController', 'method' => 'showAction'],
+        ['id' => '[0-9]+']
+    ));
